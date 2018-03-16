e6b6492471ba29811eea74072b7a96374ab1f2cd
github username <github email address>
2018-03-16 16:22:24 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+// up, up, down, down, left, right, left, right, b, a = 10
+
+let index = 0;
+
+
+
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  document.addEventListener("keydown", onKeyDownhandler) // Listens for the user input commands
+  function  onKeyDownhandler(event) {
+    const key = parseInt(event.detail || event.which);
+    (key === code[index]) ? index++ : index = 0
+     if (index === code.length){
+      alert ('i hate arren');
+    document.removeEventListener("keydown", onKeyDownhandler)
+    index = 0;
+    }
+  }
 }
